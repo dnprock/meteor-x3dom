5c016cc4cd002098caf492f73fe60ce7d00a4555
Phuoc Do <dnprock@gmail.com>
2014-01-11 22:36:09 -0800

    Update package

--- package.js
+++ package.js
@@ -3,12 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use('jquery', 'client');
-  api.add_files([
-      'lib/x3dom/x3dom-full.js'
-    ]
-    ,'client'
-  );
+  api.export('x3dom', 'client');
 
-  api.export(['x3dom'], 'client');
+  api.add_files(
+     'lib/x3dom/x3dom-full.debug.modified.js','client'
+  );
 });
