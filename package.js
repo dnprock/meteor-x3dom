56bd358b9e5401fc2321bfa17c8a1d51129ae7e5
Phuoc Do <dnprock@gmail.com>
2014-01-11 16:46:31 -0800

    Add x3dom code

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "x3dom package for meteor"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+  api.add_files([
+      'lib/x3dom/x3dom-full.js'
+    ]
+    ,'client'
+  );
+});
